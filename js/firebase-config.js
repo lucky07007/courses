a724ac789c0dee207c3b1ca9ae261a99d288f821
lucky07007 <lucky7840079250@gmail.com>
2025-11-09 16:43:34 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,23 @@
+// js/firebase-config.js
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+
+// **YOUR FIREBASE CONFIGURATION GOES HERE**
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Use this for simple access in other files
+export default app;
